b6f184858df55e484666090079ead1fec83fdc68
Jannis R <mail@jannisr.de>
2018-01-19 22:42:18 +0100

    implementation :sparkles:

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 'use strict'
 
-// todo
+const AirPlay = require('airplay-protocol')
+
+const ADDRESS = 'Apple-TV-3rd-floor.local'
+
+const play = (audioUrl, cb) => {
+	const device = new AirPlay(ADDRESS)
+
+	device.play(audioUrl, (err) => {
+		if (err) return cb(err)
+
+		const waitForPlay = () => {
+			device.playbackInfo((err, res, playbackInfo) => {
+				if (err) return cb(err)
+
+				if (playbackInfo && playbackInfo.readyToPlay) {
+					getInfo()
+					cb()
+				} else setTimeout(waitForPlay, 500)
+			})
+		}
+		waitForPlay()
+
+		const getInfo = () => {
+			device.playbackInfo((err, res, playbackInfo) => {
+				setTimeout(getInfo, 3000)
+
+				if (err) device.emit('error', err)
+				else device.emit('playbackInfo', playbackInfo)
+			})
+		}
+	})
+
+	return device
+}
+
+module.exports = play
